72cf2b1509f5afd35bbb567fd34238b55f1026a1
De Meng <mengde1120@sina.com>
2018-11-13 17:27:28 +0800

    update event handle

--- event-handle/src/components/EventHandle.vue
+++ event-handle/src/components/EventHandle.vue
@@ -21,12 +21,28 @@
                     <td>{{index + 1}}</td>
                     <td>{{name}}</td>
                     <td>
-                        <button class="btn btn-sm bg-primary text-white" v-on:click="handleClick(name)">
-                            Select
+                        <button class="btn btn-sm bg-primary text-white" v-on:click="handleClick(name)"
+                        v-on:mousemove="handleMouseEvent1(name, $event)" v-on:mouseleave="handleMouseEvent1(name, $event)">
+                            Select1
+                        </button>
+                          <button class="btn btn-sm bg-primary text-white" v-on="buttonEvents" v-bind:data-name="name">
+                            Select2
                         </button>
                     </td>
                 </tr>
             </table>
+        </div>
+        <div class="container-fluid3">
+            <h2 class="bg-primary text-white text-center mt-2 p-2">{{ message2 }}</h2>
+            <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click="handleClick2">
+                Outer Element
+                    <div id="middle-element" class="bg-secondary p-4" v-on:click="handleClick2">
+                        Middle Element
+                            <div id="inner-element" class="bg-info p-4" v-on:click="handleClick2">
+                                Inner Element
+                            </div>
+                    </div>
+            </div>
         </div>
   </div>
 </template>
@@ -41,7 +57,14 @@ export default {
       return {
           name: "Apple",
           message: "Ready",
-          names: ["Apple", "Peach", "Melon", "Banana"]
+          names: ["Apple", "Peach", "Melon", "Banana"],
+          counter: 0,
+          buttonEvents: {
+              click: this.handleClickEvent,
+              mousemove: this.handleMouseEvent2,
+              mouseleave: this.handleMouseEvent2
+          },
+          message2: "Nothing"
       }
   },
   methods: {
@@ -52,8 +75,33 @@ export default {
           //this.name = '${name} - ${$event.type}';
           this.name = name + "-" + $event.type;
       },
-      handleClick(name) {
-          this.message = "Select: " + name;
+      handleClick(name) {          
+          this.message = "Select 1: " + name;
+      },
+      handleMouseEvent1(name, $event) {
+          if ($event.type == "mousemove") {
+              this.message = "Mouse Move 1: in " + name + this.counter++;
+          } else {
+              this.count = 0;
+              this.message = "Ready";
+          }
+      },
+      handleClickEvent($event) {
+          let name = $event.target.dataset.name;
+          this.message = "Select 2: " + name;
+      },
+      handleMouseEvent2($event) {
+          let name = $event.target.dataset.name;
+          if ($event.type == "mousemove") {
+              this.message = "Mouse Move 2: in " + name + this.counter++;
+          } else {
+              this.count = 0;
+              this.message = "Ready";
+          }
+      },
+      handleClick2($event) {
+          //console.log("handleClick2 target: " + $event.target.id + "CurrentTarget: " + $event.currentTarget.id);
+          this.message2 = "handleClick2 target: " + $event.target.id + " CurrentTarget: " + $event.currentTarget.id;
       }
   }
 }
