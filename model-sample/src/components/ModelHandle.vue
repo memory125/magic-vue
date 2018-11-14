7812b5e4858a7e8acb26dbf5b66385560a181f06
De Meng <mengde1120@sina.com>
2018-11-14 10:50:41 +0800

    update model handle sample

--- model-sample/src/components/ModelHandle.vue
+++ model-sample/src/components/ModelHandle.vue
@@ -1,19 +1,63 @@
 <template>
   <div class="container-fluid">
     <div class="bg-info m-2 p-2 text-white">
-        Value: {{ dataValue }}
+        Value: {{ dataValue1 }}
     </div>
     <div class="bg-primary m-2 p-2 text-white">
         <div class="form-check">
             <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" v-on:change="handleChange"/>
+                <input class="form-check-input" type="checkbox" v-on:change="handleChange1"/>
                 Data Value
             </label>
         </div>     
     </div>
     <div class="text-center m-2">
-        <button class="btn btn-secondary" v-on:click="reset">
-            Reset
+        <button class="btn btn-secondary" v-on:click="reset1">
+            Reset1
+        </button>
+    </div>
+    <div class="bg-info m-2 p-2 text-white">
+        Data Value: {{ dataValue2 }}
+    </div>
+    <div class="bg-info m-2 p-2 text-white">
+        Other Value: {{ otherValue1 }}
+    </div>
+    <div class="bg-primary m-2 p-2 text-white">
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" v-on:change="handleChange2" v-bind:checked="dataValue2"/>
+                Data Value
+            </label>
+        </div>     
+    </div>
+    <div class="bg-primary m-2 p-2">       
+        <input type="text" class="form-control" v-on:input="handleChange2" v-bind:value="otherValue1" />
+    </div>
+    <div class="text-center m-2">
+        <button class="btn btn-secondary" v-on:click="reset2">
+            Reset2
+        </button>
+    </div>
+    <div class="bg-info m-2 p-2 text-white">
+        Data Value: {{ dataValue3 }}
+    </div>
+    <div class="bg-info m-2 p-2 text-white">
+        Other Value: {{ otherValue2 }}
+    </div>
+    <div class="bg-primary m-2 p-2 text-white">
+        <div class="form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" v-model="dataValue3" />
+                Data Value
+            </label>
+        </div>     
+    </div>
+    <div class="bg-primary m-2 p-2">       
+        <input type="text" class="form-control" v-model="otherValue2" />
+    </div>
+    <div class="text-center m-2">
+        <button class="btn btn-secondary" v-on:click="reset3">
+            Reset3
         </button>
     </div>
   </div>
@@ -27,15 +71,34 @@ export default {
   },
   data() {
       return {
-          dataValue: false
+          dataValue1: false,
+          dataValue2: false,
+          dataValue3: false,
+          otherValue1: "",
+          otherValue2: ""
       }
   },
   methods: {
-      reset() {
-          this.dataValue = false;
+      reset1() {
+          this.dataValue1 = false;
       },
-      handleChange($event) {
-          this.dataValue = $event.target.checked;
+      handleChange1($event) {
+          this.dataValue1 = $event.target.checked;
+      },
+      reset2() {
+          this.dataValue2 = false;
+          this.otherValue = "";
+      },
+      handleChange2($event) {
+          if ($event.target.type == "checkbox") {
+              this.dataValue2 = $event.target.checked;
+          } else {
+              this.otherValue = $event.target.value
+          }          
+      },
+      reset3() {
+          this.dataValue3 = false;
+          this.otherValue2 = "";
       }
   }
 }
