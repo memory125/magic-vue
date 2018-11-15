c1d36a94e9468f2feb3a9f1d968ae4bc3b9dbd70
De Meng <mengde1120@sina.com>
2018-11-15 14:05:40 +0800

    modify validationRules.js

--- model-sample/src/validationRules.js
+++ model-sample/src/validationRules.js
@@ -1,35 +1,35 @@
 function required(name) {
     return {
         validator: (value) => value != "" && value !== undefined && value !== null,
-        messgae: "A value is required for " + name
+        message: "A value is required for " + name
     }
 }
 
 function minLength(name, minlength) {
     return {
         validator: (value) => String(value).length >= minlength,
-        messgae: "At least " + minlength + " characters are required for " + name
+        message: "At least " + minlength + " characters are required for " + name
     }
 }
 
 function alpha(name) {
     return {
         validator: (value) => /^[a-zA-Z]*$/.test(value),
-        messgae: name + " can only contain letters"
+        message: name + " can only contain letters"
     }
 }
 
 function numeric(name) {
     return {
         validator: (value) => /^[0-9]*$/.test(value),
-        messgae: name + " can only contain digits"
+        message: name + " can only contain digits"
     }
 }
 
 function range(name, min, max) {
     return {
         validator: (value) => value >= min && value <= max,
-        messgae: name + " must be between " + min + " and " + max
+        message: name + " must be between " + min + " and " + max
     }
 }
 
