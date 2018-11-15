6030ef5283b8dbcc51dfd99323f85a34ab997c93
De Meng <mengde1120@sina.com>
2018-11-15 17:34:41 +0800

    add child component

---
+++ components-data/src/components/ChildComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-secondary text-white text-center m-2 p-2 h6">
+      Child Component
+      <div class="bg-primary text-white m-2 p-3">
+          {{ message }}
+      </div>
+      <div class="form-group m-1">
+          <input v-model="message" class="form-control" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+        message: "This is the child component!"
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    div {border: 5px solid lightcyan;}
+
+</style>
--- components-data/src/components/ComponentsData.vue
+++ components-data/src/components/ComponentsData.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="bg-primary text-white text-center m-2 p-2 h5">
-      Root Component
+      Root Component    
+      <div class="bg-primary text-white m-2 p-3">
+          {{ message }}
+      </div>  
+      <ChildComponent></ChildComponent>
+      <child-component></child-component>
+      <MyFeature></MyFeature>
+      <my-feature></my-feature>
   </div>
 </template>
 
 <script>
+import ChildComponent from "./ChildComponent.vue"
 export default {
   name: 'ComponentsData',
   props: {
     msg: String
+  },
+  components: {
+      ChildComponent,
+      MyFeature: ChildComponent
+  },
+  data() {
+      return {
+          message: "This is the parent component!"
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    div {border: 8px solid sandybrown}
 </style>
